2abafb514e0458656ea0312a7b9d78098b2ad814
Vladimir Starkov <iamstarkov@gmail.com>
2017-10-24 21:25:54 +0200

    rework build-scripts

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -6,78 +6,20 @@ const files = [
   'README.md',
   'CHANGELOG.md',
   'LICENSE',
+  'package.json',
 ];
 
-Promise.all(
-  files.map((file) => copyFile(file))
-)
-.then(() => createPackageFile());
+const resolveBuildPath = file =>
+  path.resolve(__dirname, '../dist/', path.basename(file));
 
-function copyFile(file) {
-  const buildPath = resolveBuildPath(file);
-  return new Promise((resolve) => {
-    fse.copy(
-      file,
-      buildPath,
-      (err) => {
-        if (err) throw err;
-        resolve();
-      }
-    );
-  })
-  .then(() => console.log(`Copied ${file} to ${buildPath}`));
-}
-
-function resolveBuildPath(file) {
-  return path.resolve(__dirname, '../build/', path.basename(file));
-}
-
-function createPackageFile() {
-  return new Promise((resolve) => {
-    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-
-      resolve(data);
-    });
-  })
-  .then((data) => JSON.parse(data))
-  .then((packageData) => {
-    const {
-      author,
-      version,
-      description,
-      keywords,
-      repository,
-      license,
-      bugs,
-      peerDependencies,
-      dependencies,
-    } = packageData;
-
-    const minimalPackage = {
-      name: 'jss-theme-reactor',
-      author,
-      version,
-      description,
-      main: './index.js',
-      keywords,
-      repository,
-      license,
-      bugs,
-      peerDependencies,
-      dependencies,
-    };
-
-    return new Promise((resolve) => {
-      const buildPath = path.resolve(__dirname, '../build/package.json');
-      const data = JSON.stringify(minimalPackage, null, 2);
-      fse.writeFile(buildPath, data, (err) => {
-        if (err) throw (err);
-        console.log(`Created package.json in ${buildPath}`);
-        resolve();
-      });
-    });
+const copyFile = file => new Promise((resolve) => {
+  fse.copy(file, resolveBuildPath(file), (err) => {
+    if (err) throw err;
+    resolve();
   });
-}
+})
+
+
+Promise.all(files.map(copyFile))
+  .then(() => console.log('Success'))
+  .catch(console.error);
