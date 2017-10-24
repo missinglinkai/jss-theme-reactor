625ae5ef938c2b29807bf6a961c94b5bb4db106f
Vladimir Starkov <iamstarkov@gmail.com>
2017-10-24 20:17:00 +0200

    prepare for react@16

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -1,6 +1,7 @@
 // @flow
 
-import { Component, PropTypes } from 'react';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 import { create } from 'jss';
 import jssPreset from 'jss-preset-default';
 import { createStyleManager } from './styleManager';
